837da8edcc7558be554543f750c5638773bab6f2
Iamneo <techadmin@iamneo.ai>
2023-08-06 08:40:19 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -3,9 +3,28 @@ import './App.css';
 
 //
 import {useState} from "react";
-
+import Button from './components/UI/Button/Button';
+import Card from './components/UI/Card/Card';
+import Banner from './components/UI/Banner/Banner';
+//
 
 function App() {
+  //
+  const [start,setStart] = useState(true);
+  const [attempt, correctAnswerMarkUpdate] = useState(0);
+  const [qsnAttempt, setqsnAttempt] = useState(0);
+  const [card, setCard] = useState(false);
+  const [result, setResult] = useState(false);
+  const [banner, setBanner] = useState(false);
+
+  const handleStart = () =>{
+    setStart(false);
+    setCard(true);
+    correctAnswerMarkUpdate(0);
+    
+  }
+  //
+  
   return (
     <div className="App">
       <header className="App-header">
